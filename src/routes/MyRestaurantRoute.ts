ce80d258b56d1d6a267d2d6955f27c0a0aa52fcd
psolanki-dev <psolanki.dev@outlook.com>
2024-05-23 16:31:55 +0100

    adding checkout flow

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -6,26 +6,46 @@ import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-
 const storage = multer.memoryStorage();
 const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 5 * 1024 * 1024,
-    },
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, 
+  },
 });
+
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
 
 router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
-router.post("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, MyRestaurantController.createMyRestaurant);
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
 
 router.put(
-    "/",
-    upload.single("imageFile"),
-    validateMyRestaurantRequest,
-    jwtCheck,
-    jwtParse,
-    MyRestaurantController.updateMyRestaurant
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
 );
 
 export default router;
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -5,7 +5,15 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
+
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
-router.post("/", jwtCheck,  MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
+
 export default router;
